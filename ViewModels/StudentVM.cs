8e1b52e069b65ee6849d5b64f3d97359ff9d1ceb
asimhussain <asimhussain254@gmail.com>
2020-12-05 17:06:16 +0500

    Sorting

---
+++ ViewModels/StudentVM.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class StudentVM
+{
+    public int ID { get; set; }
+    public string LastName { get; set; }
+    public string FirstMidName { get; set; }
+    public DateTime EnrollmentDate { get; set; }
+}
